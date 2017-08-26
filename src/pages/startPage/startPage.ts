61ef5366b1cb2706efeb6367947ba53cc72e2564
NiralSundar <sundar.raj37@gmail.com>
2017-08-26 22:50:10 +0530

    Ionic App Started

---
+++ src/pages/startPage/startPage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'start-page',
+  templateUrl: 'startPage.html'
+})
+export class StartPage {
+
+  constructor() {
+
+  }
+
+}
